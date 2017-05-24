d6944c0ecda697ba21097bcb01ebb907ec048209
Per Bruno <per.bruno@kungsbacka.se>
2017-05-24 14:58:51 +0200

    Ändring

---
+++ LovskolaRapport/IRapport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace KBA.Etjanster.Lovskola
+{
+    [ServiceContract]
+    public interface IRapport
+    {
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetData", Method = "POST")]
+        string GetData(int value);
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "GetInfo", Method = "GET")]
+        string GetInfo();
+
+        [OperationContract]
+        [WebInvoke(Method = "OPTIONS", UriTemplate = "*")]
+        void GetOptions();
+
+    }
+
+}
--- LovskolaRapport/Rapport.svc.cs
+++ LovskolaRapport/Rapport.svc.cs
@@ -6,28 +6,25 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace LovskolaRapport
+namespace KBA.Etjanster.Lovskola
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class Rapport : IRapport
     {
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public string GetInfo()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "Sanningen är en citronmaräng!!!";
+        }
+
+        public void GetOptions()
+        {
+
         }
     }
 }
